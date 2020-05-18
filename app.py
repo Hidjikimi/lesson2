670ea97193faf129ec16eacc5fb958bafcb33579
Igor Stangrit <stangrit@etcetera.kiev.ua>
2020-05-18 15:41:04 +0300

    fix #1

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, Response
 
 app = Flask(__name__)
@@ -15,4 +17,5 @@ def get_file():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
